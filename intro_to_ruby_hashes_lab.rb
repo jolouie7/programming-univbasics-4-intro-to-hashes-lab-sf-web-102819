2ca054f6781590d123ba666291e5097236783a15
Joseph  Louie <joseph161216@gmail.com>
2019-10-20 06:40:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,11 +42,11 @@ def update_counting_hash(hash, key)
   {
     "hello" => 1
   }
-  # if hash[key]
-  #   hash[key] += 1
-  # else
-  #   hash[key] = 1
-  # end
+  if hash[key]
+    hash[key] += 1
+  else
+    hash[key] = 1
+  end
 end
 
 
